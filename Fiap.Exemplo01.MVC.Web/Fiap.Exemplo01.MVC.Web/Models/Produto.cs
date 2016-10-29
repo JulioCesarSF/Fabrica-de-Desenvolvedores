f6845a5f85124a95e1159a728a020c82a58d78af
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-10-28 23:56:52 -0200

    Fix Produto Model com Display

--- Fiap.Exemplo01.MVC.Web/Fiap.Exemplo01.MVC.Web/Models/Produto.cs
+++ Fiap.Exemplo01.MVC.Web/Fiap.Exemplo01.MVC.Web/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace Fiap.Exemplo01.MVC.Web.Models
 {
     public class Produto
     {
+        [Display (Name ="Nome")]
         public string Nome { get; set; }
+        [Display (Name = "Quantidade")]
         public int Quantidade { get; set; }
+        [Display (Name = "Idade")]
         public decimal Valor { get; set; }
 
         public Produto()
