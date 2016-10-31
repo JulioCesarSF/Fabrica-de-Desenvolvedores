eaaef0ef1409dd9c863d745353772fe5e05d6f5e
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-10-31 21:04:25 -0200

    Refeita lista de EstadoCivil com IList<SelectListItem> _estCivil

--- Fiap.Exemplo01.MVC.Web/Fiap.Exemplo01.MVC.Web/Models/Cliente.cs
+++ Fiap.Exemplo01.MVC.Web/Fiap.Exemplo01.MVC.Web/Models/Cliente.cs
@@ -4,37 +4,43 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Fiap.Exemplo01.MVC.Web.Models
 {
     public class Cliente
     {
         //lista de EstadoCivil com inicialização https://msdn.microsoft.com/en-us/library/bb384062.aspx        
-        public IList<string> _estadoCivil = new List<string>()
-        {
-           "Selecione",
-           "Solteiro",
-           "Casado",
-           "Outro"
-        };
-        [Display (Name = "Nome")]
+        //public IList<string> _estadoCivil = new List<string>()
+        //{
+        //   "Selecione",
+        //   "Solteiro",
+        //   "Casado",
+        //   "Outro"
+        //};
+
+        //Nova lista de SelectListItem conforme doc da MS: 
+        //https://www.asp.net/mvc/overview/older-versions/working-with-the-dropdownlist-box-and-jquery/using-the-dropdownlist-helper-with-aspnet-mvc
+        public IList<SelectListItem> _estCivil = new List<SelectListItem>();
+
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
-        [Display (Name = "Idade")]
+        [Display(Name = "Idade")]
         public int Idade { get; set; }
-        [Display (Name = "Crédito")]
+        [Display(Name = "Crédito")]
         public decimal Credito { get; set; }
-        [Display (Name = "Necessidades Especiais")]
+        [Display(Name = "Necessidades Especiais")]
         public bool NecessidadesEspeciais { get; set; }
-        [Display (Name = "Estado Civil")]
+        [Display(Name = "Estado Civil")]
         public string EstadoCivil { get; set; }
-        [Display (Name = "Data de Nascimento")]
+        [Display(Name = "Data de Nascimento")]
         [UIHint("Date")]
         public DateTime DataNascimento { get; set; }
 
         public Cliente()
         {
 
-        }        
+        }
 
         public Cliente(string nome, int idade, decimal credito, bool necessidadesEspeciais, string estadoCivil, DateTime dataNascimento)
         {
@@ -44,7 +50,13 @@ namespace Fiap.Exemplo01.MVC.Web.Models
             NecessidadesEspeciais = necessidadesEspeciais;
             EstadoCivil = estadoCivil;
             DataNascimento = dataNascimento;
+
+            //inicialização da lista de SelectListItem
+            _estCivil.Add(new SelectListItem { Text = "Selecione", Value = "0" });
+            _estCivil.Add(new SelectListItem { Text = "Solteiro", Value = "1" });
+            _estCivil.Add(new SelectListItem { Text = "Casado", Value = "2" });
+            _estCivil.Add(new SelectListItem { Text = "Outro", Value = "3" });
         }
-        
+
     }
 }
