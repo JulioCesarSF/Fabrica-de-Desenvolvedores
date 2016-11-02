9801dd0a8ebb68878e95e113328feed9e7e00f17
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-11-01 22:25:17 -0200

    Criacao de HomeController e View/Home

---
+++ Fiap.Exercicio03.MVC.Web/Fiap.Exercicio03.MVC.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,19 @@
+﻿using Fiap.Exercicio03.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exercicio03.MVC.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private ProdutoContext pContext = new ProdutoContext();
+        // GET: Produto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
