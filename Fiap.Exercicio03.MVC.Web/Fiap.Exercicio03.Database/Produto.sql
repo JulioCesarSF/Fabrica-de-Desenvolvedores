ae33ff646833df26cd77df64743c813e3eca8d72
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-11-01 22:08:28 -0200

    Criacao da tabela Produto

---
+++ Fiap.Exercicio03.MVC.Web/Fiap.Exercicio03.Database/Produto.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Produto]
+(
+	[ProdutoId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [DataCadastro] DATE NOT NULL, 
+    [Descricao] NVARCHAR(200) NOT NULL, 
+    [Nacional] BIT NOT NULL, 
+    [Titulo] NVARCHAR(150) NOT NULL, 
+    [Valor] DECIMAL(10, 2) NOT NULL
+)
