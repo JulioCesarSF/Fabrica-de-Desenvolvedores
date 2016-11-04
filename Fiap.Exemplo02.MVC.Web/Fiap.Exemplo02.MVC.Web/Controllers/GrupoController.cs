057a93f890766657e6a3360f414317fc9ad6d99f
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-04 18:21:01 -0200

    Cadastro e listagem de grupo

---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.MVC.Web/Controllers/GrupoController.cs
@@ -0,0 +1,36 @@
+﻿using Fiap.Exemplo02.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo02.MVC.Web.Controllers
+{
+    public class GrupoController : Controller
+    {
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Grupo grupo)
+        {
+            var con = new PortalContext();
+            con.Grupo.Add(grupo);
+            con.SaveChanges();
+            TempData["tipoMensagem"] = "alert alert-success";
+            TempData["mensagem"] = "Cadastrado com sucesso!";
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            var con = new PortalContext();
+            var l = con.Grupo.ToList();
+            return View(l);
+        }
+    }
+}
