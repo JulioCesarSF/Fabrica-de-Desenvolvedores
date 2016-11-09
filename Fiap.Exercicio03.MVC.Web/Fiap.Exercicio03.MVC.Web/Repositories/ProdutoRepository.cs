2f8167c101799594fdf3544072311113ed26adf4
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-11-09 15:55:49 -0200

    Add ProdutoRepository

---
+++ Fiap.Exercicio03.MVC.Web/Fiap.Exercicio03.MVC.Web/Repositories/ProdutoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exercicio03.MVC.Web.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+
+    }
+}
