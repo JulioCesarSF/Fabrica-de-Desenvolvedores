29e479dd1696c6bd21cefbb451907eb3f299e399
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-11-09 14:35:13 -0200

    Pasta Repositories, interface IProdutoRepository

---
+++ Fiap.Exercicio03.MVC.Web/Fiap.Exercicio03.MVC.Web/Repositories/IProdutoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exercicio03.MVC.Web.Repositories
+{
+    public interface IProdutoRepository
+    {
+        void Cadastrar(Produto produto);
+        void Atualizar(Produto produto);
+        void Remover(int id);
+        Produto BuscarPorId(int id);
+        ICollection<Produto> Listar();
+        ICollection<Produto> BuscarPorId(Expression<Func<Produto, bool>> filtro);
+    }
+}
