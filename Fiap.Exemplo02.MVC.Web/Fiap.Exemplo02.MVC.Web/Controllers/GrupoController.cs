1a932ac8819c4805d0760615acb86992c3dad2bb
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-11-09 15:32:41 -0200

    Added IProjetoRepository.cs

--- Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.MVC.Web/Controllers/GrupoController.cs
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.MVC.Web/Controllers/GrupoController.cs
@@ -1,4 +1,5 @@
 ﻿using Fiap.Exemplo02.MVC.Web.Models;
+using Fiap.Exemplo02.MVC.Web.UnitsOfWork;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -10,6 +11,7 @@ namespace Fiap.Exemplo02.MVC.Web.Controllers
 {
     public class GrupoController : Controller
     {
+        private UnitOfWork _unit = new UnitOfWork();
         [HttpGet]
         public ActionResult Cadastrar()
         {
@@ -19,9 +21,11 @@ namespace Fiap.Exemplo02.MVC.Web.Controllers
         [HttpPost]
         public ActionResult Cadastrar(Grupo grupo)
         {
-            var con = new PortalContext();
-            con.Grupo.Add(grupo);
-            con.SaveChanges();
+            //var con = new PortalContext();
+            //con.Grupo.Add(grupo);
+            //con.SaveChanges();
+            _unit.GrupoRepository.Cadastrar(grupo);
+            _unit.Save();
             TempData["tipoMensagem"] = "alert alert-success";
             TempData["mensagem"] = "Cadastrado com sucesso!";
             return View();
@@ -30,33 +34,36 @@ namespace Fiap.Exemplo02.MVC.Web.Controllers
         [HttpGet]
         public ActionResult Listar()
         {
-            var con = new PortalContext();
-            var l = con.Grupo.ToList();
-            return View(l);
+            //var con = new PortalContext();
+            //var l = con.Grupo.ToList();
+            
+            return View(_unit.GrupoRepository.Listar());
         }
 
         [HttpGet]
         public ActionResult Alterar(int id)
         {
-            var con = new PortalContext();
-            var g = con.Grupo.Find(id);
-            return View(g);
+            //var con = new PortalContext();
+            //var g = con.Grupo.Find(id);
+            return View(_unit.GrupoRepository.BuscarPorId(id));
         }
 
         [HttpPost]
         public ActionResult Alterar(Grupo grupo)
         {
-            var con = new PortalContext();
-            var projeto = con.Projeto.Find(grupo.Id);
-            var g = con.Grupo.Find(grupo.Id);
-            projeto.Nome = grupo.Projeto.Nome;
-            projeto.Descricao = grupo.Projeto.Descricao;
-            projeto.DataInicio = grupo.Projeto.DataInicio;
-            projeto.DataTermino = grupo.Projeto.DataTermino;
-            projeto.Entregue = grupo.Projeto.Entregue;
-            g.Nome = grupo.Nome;
-            g.Nota = grupo.Nota;   
-            con.SaveChanges();
+            //var con = new PortalContext();
+            //var projeto = con.Projeto.Find(grupo.Id);
+            //var g = con.Grupo.Find(grupo.Id);
+            //projeto.Nome = grupo.Projeto.Nome;
+            //projeto.Descricao = grupo.Projeto.Descricao;
+            //projeto.DataInicio = grupo.Projeto.DataInicio;
+            //projeto.DataTermino = grupo.Projeto.DataTermino;
+            //projeto.Entregue = grupo.Projeto.Entregue;
+            //g.Nome = grupo.Nome;
+            //g.Nota = grupo.Nota;   
+            //con.SaveChanges();
+            _unit.GrupoRepository.Atualizar(grupo);
+            _unit.Save();
             TempData["tipoMensagem"] = "alert alert-success";
             TempData["mensagem"] = "Grupo/Projeto atualizado!";
             return RedirectToAction("Listar");
@@ -65,12 +72,14 @@ namespace Fiap.Exemplo02.MVC.Web.Controllers
         [HttpPost]
         public ActionResult Excluir(int id)
         {
-            var con = new PortalContext();
-            var projeto = con.Projeto.Find(id);
-            con.Projeto.Remove(projeto);
-            var gg = con.Grupo.Find(id);
-            con.Grupo.Remove(gg);
-            con.SaveChanges();
+            //var con = new PortalContext();
+            //var projeto = con.Projeto.Find(id);
+            //con.Projeto.Remove(projeto);
+            //var gg = con.Grupo.Find(id);
+            //con.Grupo.Remove(gg);
+            //con.SaveChanges();
+            _unit.GrupoRepository.Remover(id);
+            _unit.Save();
             TempData["tipoMensagem"] = "alert alert-success";
             TempData["mensagem"] = "Grupo/Projeto removido com sucesso!";
             return RedirectToAction("Listar");
---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.MVC.Web/Repositories/IProjetoRepository.cs
@@ -0,0 +1,20 @@
+﻿using Fiap.Exemplo02.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exemplo02.MVC.Web.Repositories
+{
+    public interface IProjetoRepository
+    {
+        void Cadastrar(Projeto aluno);
+        void Atualizar(Projeto aluno);
+        void Remover(int id);
+        Projeto BuscarPorId(int id);
+        ICollection<Projeto> Listar();
+        ICollection<Projeto> BuscarPor(Expression<Func<Projeto, bool>> filtro);
+    }
+}
