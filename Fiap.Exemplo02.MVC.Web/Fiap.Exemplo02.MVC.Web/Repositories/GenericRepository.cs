d11be6e4ed93acfb99a3a74ad2d97db3362e34ec
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-11 17:16:17 -0200

    Added GenericsRepository

---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.MVC.Web/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Fiap.Exemplo02.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo02.MVC.Web.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private PortalContext _context;
+        private DbSet<T> _dbset;
+
+        public GenericRepository(PortalContext context)
+        {
+            _context = context;
+            _dbset = _context.Set<T>();
+        }
+        public void Alterar(T entidade)
+        {
+            _context.Entry(entidade).State = EntityState.Modified;
+        }
+
+        public ICollection<T> BuscarPor(System.Linq.Expressions.Expression<Func<T, bool>> filtro)
+        {
+            return _dbset.Where(filtro).ToList();
+        }
+
+        public T BuscarPorId(int id)
+        {
+            return _dbset.Find(id);
+        }
+
+        public void Cadastrar(T entidade)
+        {
+            _context.Entry(entidade).State = EntityState.Added;
+        }
+
+        public ICollection<T> Listar()
+        {
+            return _dbset.ToList();
+        }
+
+        public void Remover(int id)
+        {
+            _dbset.Remove(BuscarPorId(id));
+        }
+    }
+}
---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.MVC.Web/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exemplo02.MVC.Web.Repositories
+{
+    public interface IGenericRepository<T>
+    {
+        void Cadastrar(T entidade);
+        void Alterar(T entidade);
+        void Remover(int id);
+        ICollection<T> Listar();
+        T BuscarPorId(int id);
+        ICollection<T> BuscarPor(Expression<Func<T, bool>> filtro);
+    }
+}
