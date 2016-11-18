2f0cd6545367950da7f540fd8a17023c6b46333b
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-18 15:09:35 -0200

    Add AlunoViewMode.cs

---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.MVC.Web/ViewModels/AlunoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exemplo02.MVC.Web.ViewModels
+{
+    public class AlunoViewModel
+    {
+        #region Aluno Properties
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Nullable<double> Desconto { get; set; }
+        public bool Bolsa { get; set; }
+        public int GrupoId { get; set; }
+        #endregion
+    }
+}
