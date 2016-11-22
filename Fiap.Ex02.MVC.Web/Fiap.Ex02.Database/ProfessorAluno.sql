b8dd0fda33d460cd5fa7cb4fbbc3b296f5aa67ec
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-22 21:04:43 -0200

    Added Model baseados no Banco no projeto web

--- Fiap.Ex02.MVC.Web/Fiap.Ex02.Database/ProfessorAluno.sql
+++ Fiap.Ex02.MVC.Web/Fiap.Ex02.Database/ProfessorAluno.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ProfessorAluno]
 (
-	[ProfessorId] INT NOT NULL PRIMARY KEY, 
+	[ProfessorId] INT NOT NULL, 
     [AlunoId] INT NOT NULL, 
     CONSTRAINT [FK_ProfessorAluno_Professor] FOREIGN KEY ([ProfessorId]) REFERENCES [Professor]([Id]), 
     CONSTRAINT [FK_ProfessorAluno_Aluno] FOREIGN KEY ([AlunoId]) REFERENCES [Aluno]([Id])
