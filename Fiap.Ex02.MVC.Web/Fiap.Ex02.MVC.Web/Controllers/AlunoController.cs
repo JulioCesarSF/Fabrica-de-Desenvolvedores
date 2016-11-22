3a5727a552ff4875ca9681992fb5ea7fefc55003
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-22 21:26:07 -0200

    Added AlunoController

---
+++ Fiap.Ex02.MVC.Web/Fiap.Ex02.MVC.Web/Controllers/AlunoController.cs
@@ -0,0 +1,26 @@
+﻿using Fiap.Ex02.MVC.Web.UnitsOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Ex02.MVC.Web.Controllers
+{
+    public class AlunoController : Controller
+    {
+        #region FIELDS
+        private UnitOfWork _unit = new UnitOfWork();
+        #endregion
+
+        #region GETS
+
+        public ActionResult Cadastrar(string mensagem, string tipoMensagem)
+        {
+            
+            return View();
+        }
+
+        #endregion
+    }
+}
