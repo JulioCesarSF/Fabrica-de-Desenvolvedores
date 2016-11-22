5d38876d894147a6ece92f768d3f16e38faa53af
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-22 21:12:46 -0200

    Added GenericRepository com Interface

---
+++ Fiap.Ex02.MVC.Web/Fiap.Ex02.MVC.Web/Repositories/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using Fiap.Ex02.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace Fiap.Ex02.MVC.Web.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected PortalContext _context;
+        protected DbSet<T> _dbset;
+
+        public GenericRepository(PortalContext context)
+        {
+            _context = context;
+            _dbset = _context.Set<T>();
+        }
+        public void Alterar(T entidade)
+        {
+            _context.Entry(entidade).State = EntityState.Modified;
+        }
+
+        public ICollection<T> BuscarPor(Expression<Func<T, bool>> filtro)
+        {
+            return _dbset.Where(filtro).ToList();
+        }
+
+        public T BuscarPorId(int id)
+        {
+            return _dbset.Find(id);
+        }
+
+        public void Cadastrar(T entidade)
+        {
+            _context.Entry(entidade).State = EntityState.Added;
+        }
+
+        public ICollection<T> Listar()
+        {
+            return _dbset.ToList();
+        }
+
+        public void Remover(int id)
+        {
+            _dbset.Remove(BuscarPorId(id));
+        }
+    }
+}
