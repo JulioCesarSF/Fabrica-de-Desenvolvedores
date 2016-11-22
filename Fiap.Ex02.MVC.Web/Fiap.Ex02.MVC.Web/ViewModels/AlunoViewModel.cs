2b50425d67b41bbc8127d12da63cb26eeac52a95
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-22 21:30:13 -0200

    Added AlunoViewModel

---
+++ Fiap.Ex02.MVC.Web/Fiap.Ex02.MVC.Web/ViewModels/AlunoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Ex02.MVC.Web.ViewModels
+{
+    public class AlunoViewModel
+    {
+        public SelectList ListaGrupo { get; set; }
+        public string Mensagem { get; set; }
+        public string TipoMensagem { get; set; }
+
+        #region PROPERTIES
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento{ get; set; }
+        public Nullable<double> Desconto { get; set; }
+        public bool Bolsa { get; set; }
+        public int GrupoId { get; set; }
+        #endregion
+
+    }
+}
