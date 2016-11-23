a7bfe50d9e9fb3bba43ffa705b95d760d4f7f006
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-23 19:11:04 -0200

    Add ProjetoViewModel e ProjetoController

---
+++ Fiap.Ex02.MVC.Web/Fiap.Ex02.MVC.Web/Controllers/ProjetoController.cs
@@ -0,0 +1,71 @@
+﻿using Fiap.Ex02.MVC.Web.Models;
+using Fiap.Ex02.MVC.Web.UnitsOfWork;
+using Fiap.Ex02.MVC.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Ex02.MVC.Web.Controllers
+{
+    public class ProjetoController : Controller
+    {
+        #region FIELDS
+        private UnitOfWork _unit = new UnitOfWork();
+        #endregion
+
+        #region GETS
+
+        [HttpGet]
+        public ActionResult Cadastrar(string mensagem, string tipoMensagem)
+        {
+            var viewModel = new ProjetoViewModel()
+            {
+                Mensagem = mensagem,
+                TipoMensagem = tipoMensagem,
+                Grupos = ListarGrupos()
+            };
+            return View(viewModel);
+        }
+
+        #endregion
+
+        #region POSTS
+
+        [HttpPost]
+        public ActionResult Cadastrar(ProjetoViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var projeto = new Projeto()
+                {
+                    Nome = viewModel.Nome,
+                    DataInicio = viewModel.DataInicio,
+                    DataTermino = viewModel.DataTermino,
+                    Grupo = _unit.GrupoRepository.BuscarPorId(viewModel.GrupoId)
+                };
+
+                _unit.ProjetoRepository.Cadastrar(projeto);
+                _unit.Save();
+
+                return RedirectToAction("Cadastrar", 
+                    new { mensagem = "Cadastro Realizado!", tipoMensagem = "alert alert-success" });
+            }else
+            {
+                viewModel.Grupos = ListarGrupos();
+                return View(viewModel);
+            }           
+        }
+
+        #endregion
+
+        #region PRIVATE
+
+        public SelectList ListarGrupos()
+        {
+            return new SelectList(_unit.GrupoRepository.Listar(), "Id", "Nome");
+        }
+        #endregion
+    }
+}
--- Fiap.Ex02.MVC.Web/Fiap.Ex02.MVC.Web/ViewModels/ProjetoViewModel.cs
+++ Fiap.Ex02.MVC.Web/Fiap.Ex02.MVC.Web/ViewModels/ProjetoViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,14 +9,23 @@ namespace Fiap.Ex02.MVC.Web.ViewModels
 {
     public class ProjetoViewModel
     {
-
         public SelectList Grupos { get; set; }
+        public string Mensagem { get; set; }
+        public string TipoMensagem { get; set; }
+
 
         #region FIELDS
 
         public int Id { get; set; }
+        [Display(Name = "Nome do Projeto")]
+        [Required]
+        [StringLength(150)]
         public string Nome { get; set; }
+        [Display(Name = "Data de Início")]
+        [Required]
         public DateTime DataInicio { get; set; }
+        [Display(Name = "Data de Término")]
+        [Required]
         public DateTime DataTermino { get; set; }
         public int GrupoId { get; set; }
 
