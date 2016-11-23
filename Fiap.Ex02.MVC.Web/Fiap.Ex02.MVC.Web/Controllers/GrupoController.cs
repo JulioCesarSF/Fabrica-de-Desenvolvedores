f93e5096ac7f4c186245fd3e3dde1e620568d29d
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-23 08:50:55 -0200

    Add GrupoController

---
+++ Fiap.Ex02.MVC.Web/Fiap.Ex02.MVC.Web/Controllers/GrupoController.cs
@@ -0,0 +1,25 @@
+﻿using Fiap.Ex02.MVC.Web.UnitsOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Ex02.MVC.Web.Controllers
+{
+    public class GrupoController : Controller
+    {
+        #region FIELDS
+        private UnitOfWork _unit = new UnitOfWork();
+        #endregion
+
+        #region GETS
+
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
