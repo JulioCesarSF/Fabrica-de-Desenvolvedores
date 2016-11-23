1b08b55ae3ea8909a95082929ba4c1851a552ee1
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-23 18:57:51 -0200

    Add ProjetoViewModel

---
+++ Fiap.Ex02.MVC.Web/Fiap.Ex02.MVC.Web/ViewModels/ProjetoViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Ex02.MVC.Web.ViewModels
+{
+    public class ProjetoViewModel
+    {
+
+        public SelectList Grupos { get; set; }
+
+        #region FIELDS
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataTermino { get; set; }
+        public int GrupoId { get; set; }
+
+        #endregion
+    }
+}
