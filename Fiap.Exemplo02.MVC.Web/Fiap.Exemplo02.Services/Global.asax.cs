6dd3d8c73b8b2b0ed2d1f6f7ada4f56dba0cd09f
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-29 17:54:32 -0200

    Del WebApiConfig.cs do Service

--- Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.Services/Global.asax.cs
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.Services/Global.asax.cs
@@ -11,7 +11,7 @@ namespace Fiap.Exemplo02.Services
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
