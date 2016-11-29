400f010feeb04a6b354b2413c51c1f3c922b9702
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-11-29 17:37:45 -0200

    Add projeto de Service

---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.Services/Controllers/AlunoController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Fiap.Exemplo02.Services.Controllers
+{
+    public class AlunoController : ApiController
+    {
+    }
+}
