737a8590a30a686eaf49e23db66d6cc7bf419bb6
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-06 18:36:47 -0200

    Teste cadastro de prof

---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.Persistencia.Test/GenericRepositoryTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fiap.Exemplo02.MVC.Web.Repositories;
+using Fiap.Exemplo02.Dominio.Models;
+
+namespace Fiap.Exemplo02.Persistencia.Test
+{
+    [TestClass]
+    public class GenericRepositoryTest
+    {
+        private GenericRepository<Professor> _repository;
+        private PortalContext _context;
+
+        [TestInitialize]
+        public void Inicializacao()
+        {
+            //Inicializar objetos
+            _context = new PortalContext();
+            _repository = new GenericRepository<Professor>(_context);
+        }
+
+        /*
+        No caso do Aluno, cadastrar um grupo dentro do teste também
+        */
+
+        [TestMethod]
+        public void Cadastro_Professor_Ok()
+        {
+            var prof = new Professor()
+            {
+                Nome = "Professor Test",
+                Salario = 2132
+            };
+            _repository.Cadastrar(prof);
+            int r = _context.SaveChanges();
+            Assert.AreEqual(1, r);
+        }
+    }
+}
---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo02.Persistencia.Test/ProfessorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exemplo02.Persistencia.Test
+{
+    public class ProfessorTest
+    {
+        public void Cadastro_Professor_Ok()
+        {
+            
+        }
+    }
+}
