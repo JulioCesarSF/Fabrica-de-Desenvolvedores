bf9928b058d677d65ad7f7983b9c414ae287143d
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-06 17:14:10 -0200

    Cadastrar/Listar Professor com Ajax OK

---
+++ Fiap.Exemplo04.Web.MVC/Fiap.Exemplo04.Web.MVC/ViewModels/ProfessorViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exemplo04.Web.MVC.ViewModels
+{
+    public class ProfessorViewModel
+    {
+        #region FIELDs
+        [StringLength(100)]
+        [Required]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Required]
+        [Display(Name = "Salário")]
+        public Decimal Salario { get; set; }
+
+        public string Mensagem { get; set; }
+        public string TipoMensagem { get; set; }
+        #endregion
+    }
+}
