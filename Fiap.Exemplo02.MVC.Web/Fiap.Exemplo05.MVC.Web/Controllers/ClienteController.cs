386b19de6a63c4c38a562cc88780a08e5ce48b33
Julio Cesar Schincariol Filho <julio.com@uol.com.br>
2016-12-09 17:05:55 -0200

    Projeto Login

---
+++ Fiap.Exemplo02.MVC.Web/Fiap.Exemplo05.MVC.Web/Controllers/ClienteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo05.MVC.Web.Controllers
+{
+    [Authorize]//restrição de acesso para usuario logado
+    public class ClienteController : Controller
+    {
+        [HttpGet]        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
